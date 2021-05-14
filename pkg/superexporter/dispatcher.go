b45317d3eb0d81663caa16cc10e65e75e05a604a
Takuto Komazaki <takuto.komazaki@gree.net>
2021-05-14 17:58:27 +0900

    upd

--- pkg/superexporter/dispatcher.go
+++ pkg/superexporter/dispatcher.go
@@ -18,18 +18,19 @@ const (
 )
 
 type Target struct {
-	host string
-	port string
+	Host string
+	Port string
 	kind string
 }
 
 func (t *Target) id() string {
-	return t.host + "-" + t.port
+	return t.Host + "-" + t.Port
 }
 
 type WorkerInfo struct {
 	//kind   string
 	worker        *Worker
+	target        *Target
 	lastRequestAt time.Time
 }
 
@@ -48,9 +49,7 @@ func NewDispatcher() *Dispatcher {
 func (d *Dispatcher) CleanupAll() {
 	log.Println("Cleanup All Workers")
 	for _, wi := range d.workersInfo {
-		if err := DestoryWorker(wi.worker); err != nil {
-			log.Print("Cleanup Error!!!!!", err)
-		}
+		d.removeWorkerInfo(wi)
 	}
 }
 
@@ -65,25 +64,49 @@ func (srv *Dispatcher) Handler(w http.ResponseWriter, r *http.Request) {
 	}
 	host, port, _ := net.SplitHostPort(parsedUrl.Host)
 	kind := "memcached"
-	log.Print("kind:", kind, "host:", host, " port:", port)
-	target := Target{host: host, port: port, kind: kind}
+	log.Print("kind:", kind, " host:", host, " port:", port)
+	target := Target{Host: host, Port: port, kind: kind}
 
 	wi, ok := srv.workersInfo[target.id()]
 	if !ok {
-		log.Print("create new worker!")
-		worker, err := CreateWorker(&target)
+		log.Print("create new worker")
+		wi, err = srv.addWorkerInfo(&target)
 		if err != nil {
-			log.Print("err!!!", err)
+			log.Print("err:", err)
+			return
+		}
+		/* worker, err := CreateWorker(&target)
+		if err != nil {
+			log.Print("err:", err)
 			return
 		}
 		wi = &WorkerInfo{worker: worker}
-		srv.workersInfo[target.id()] = wi
+		srv.workersInfo[target.id()] = wi */
 	}
-	log.Print("let's request")
+	log.Print("do request")
 	if err := wi.worker.Request(&w, r); err != nil {
+		log.Print(err)
 		return
 	}
 	wi.lastRequestAt = time.Now()
+}
+
+func (srv *Dispatcher) addWorkerInfo(t *Target) (*WorkerInfo, error) {
+	worker, err := CreateWorker(t)
+	if err != nil {
+		log.Print("err:", err)
+		return nil, err
+	}
+	wi := &WorkerInfo{worker: worker, target: t}
+	srv.workersInfo[t.id()] = wi
+	return wi, nil
+}
+
+func (srv *Dispatcher) removeWorkerInfo(wi *WorkerInfo) {
+	if err := DestoryWorker(wi.worker); err != nil {
+		log.Print("DestoryWorker Error!", err)
+	}
+	delete(srv.workersInfo, wi.target.id())
 }
 
 func initSigHandler() {
@@ -94,18 +117,20 @@ func initSigHandler() {
 	go func() {
 		for {
 			sig := <-waits
+			syscall.Wait4(-1, nil, syscall.WNOHANG|syscall.WUNTRACED, nil)
 			fmt.Println("handle sigchld:", sig)
 		}
 	}()
 }
 
 func (srv *Dispatcher) periodicCleanup() {
-	log.Print("yeeey, periodic cleanup ")
 	now := time.Now()
 	if int(now.Sub(srv.lastCleanupAt).Seconds()) >= cleanupPeriodSeconds {
+		log.Print("periodic cleanup")
 		for _, wi := range srv.workersInfo {
 			if int(now.Sub(wi.lastRequestAt).Seconds()) >= workerInactiveThresholdSeconds {
-				defer DestoryWorker(wi.worker)
+				//defer DestoryWorker(wi.worker)
+				defer srv.removeWorkerInfo(wi)
 			}
 		}
 		srv.lastCleanupAt = now
--- pkg/superexporter/worker.go
+++ pkg/superexporter/worker.go
@@ -1,16 +1,22 @@
 package superexporter
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"os"
 	"os/exec"
+	"strings"
 	"syscall"
+	"text/template"
+	"time"
 )
 
+/* worker for memcached_exporter */
 type Worker struct {
 	pid      int
 	cmd      []string
@@ -18,13 +24,49 @@ type Worker struct {
 	client   *http.Client
 }
 
+var (
+	memcachedExporterBin     = os.Getenv("MEMCACHED_EXPORTER_BIN")
+	memcachedExporterOptions = os.Getenv("MEMCACHED_EXPORTER_OPTIONS")
+)
+
+func init() {
+	if memcachedExporterBin == "" {
+		memcachedExporterBin = "memcached_exporter"
+	}
+	log.Print("exporter bin:", memcachedExporterBin)
+	if memcachedExporterOptions == "" {
+		memcachedExporterOptions = "--web.listen-address unix://{{.SockName}} --memcached.address {{.Target.Host}}:{{.Target.Port}} --web.telemetry-path /metrics"
+	}
+	//log.Print("exporter options:", memcachedExporterOptions)
+}
+
 //func CreateWorker(cmd []string) (*Worker, error) {
 func CreateWorker(t *Target) (*Worker, error) {
 	log.Print("[worker] tgt:", t)
-	w := &Worker{cmd: []string{"sleep", "60"}, sockName: t.id()}
+	tmpl, err := template.New("optTmpl").Parse(memcachedExporterOptions)
+	if err != nil {
+		log.Print("error: memcachedExporterOptions:", memcachedExporterOptions)
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	sockName := "./_" + t.id() + ".sock"
+	err = tmpl.Execute(buf, struct {
+		SockName string
+		Target   *Target
+	}{sockName, t})
+	if err != nil {
+		return nil, err
+	}
+	optStr := buf.String()
+	log.Print("exporter options:", optStr)
+
+	//w := &Worker{cmd: []string{"sleep", "60"}, sockName: t.id()}
+	cmd := append([]string{memcachedExporterBin}, strings.Split(optStr, " ")...)
+	w := &Worker{cmd: cmd, sockName: sockName}
 	if err := w.spawn(); err != nil {
 		return nil, err
 	}
+	time.Sleep(time.Second * 1)
 	log.Print("worker created")
 	return w, nil
 }
@@ -56,10 +98,9 @@ func (w *Worker) Release() error {
 func (w *Worker) Request(writerRef *http.ResponseWriter, r *http.Request) error {
 	fmt.Println("request!!")
 	//(*writerRef).Write([]byte(`hello world`))
-	(*writerRef).Write([]byte(`hello world`))
-	return nil
+	//return nil
 
-	response, err := w.client.Get("http://unix")
+	response, err := w.client.Get("http://unix/metrics")
 	if err != nil {
 		return err
 	}
@@ -74,7 +115,7 @@ func (w *Worker) Request(writerRef *http.ResponseWriter, r *http.Request) error 
 	return nil
 }
 
-func (w Worker) initHttpClient() {
+func (w *Worker) initHttpClient() {
 
 	if w.sockName == "" {
 		panic("empty socket name")
